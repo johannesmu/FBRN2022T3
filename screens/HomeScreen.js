df81da3320f602ca9b874facb59d008f9218ee75
johannes <johannes.muljana@ait.nsw.edu.au>
2022-11-09 19:16:31 +1100

    added button to navigate to register

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 export function HomeScreen( props ) {
+    const navigation = useNavigation()
+
+    const pressHandler = () => {
+        navigation.navigate("Register")
+    }
+
     return (
         <View>
             <Text>Home </Text>
+            <TouchableOpacity style={ styles.button } onPress={ () => pressHandler() }>
+                <Text>Go to Register</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: "lightblue",
+        padding: 10,
+    }
+})
