e5c3050b355a7d4858e64814857b5216f0820956
johannes <johannes@bx.o.a>
2022-11-30 14:25:23 +1100

    added bottom tab

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,11 +2,13 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { useState, useEffect } from 'react'
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useNavigation } from '@react-navigation/native'
 
 import {ListScreen} from './ListScreen'
 
 const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
 
 export function HomeScreen( props ) {
     const navigation = useNavigation()
@@ -24,9 +26,9 @@ export function HomeScreen( props ) {
 
     return (
         
-            <Stack.Navigator>
+            <Tab.Navigator>
                 <Stack.Screen name="List" component={ListScreen} />
-            </Stack.Navigator>
+            </Tab.Navigator>
         
     )
 }
